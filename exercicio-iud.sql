1298499e22065e9512ea9df2087ff0c2a706215f
Camila Minei <camisminei.ufms@gmail.com>
2020-10-06 09:04:51 -0400

    atividade-bd

---
+++ exercicio-iud.sql
@@ -0,0 +1,5 @@
+-- 1. Insira todos os dados no banco de dados empresa (conforme definido no estudo de caso)
+
+-- 2. Remova os dependentes do funcionário cujo cpf é '333445555'.
+
+-- 3. Altere o nome do departamento 'Pesquisa' para 'Pesquisa científica'.
